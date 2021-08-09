d4b3d4ee2e88835cec991747f23f3c16f3b82327
shirley he <HEX10@miamioh.edu>
2021-08-09 12:42:36 -0400

    add ccvg as prefix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   //backend service will run on this API ROOT, which points to the server
-  API_ROOT: "http://ngrok.luozm.me:8395/",
+  API_ROOT: "http://ngrok.luozm.me:8395/ccvg/",
 };
